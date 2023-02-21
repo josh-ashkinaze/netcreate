b4821c7224f9e429ddfc13d80f11c8d6747403c9
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-02-20 20:07:29 -0500

    half broken -- consent page works, goes to expr, does not go to thank you

---
+++ website/web.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def consent_form():
+    return render_template('consent_form.html')
+
+@app.route("/experiment", methods=['GET', 'POST'])
+def experiment():
+    if request.method == 'POST':
+        # Do something with the participant's response
+        response = request.form.get('response')
+        # Render the experiment page
+        return render_template('experiment.html')
+    else:
+        # Render the consent form page
+        return render_template('consent_form.html')
+
+
+@app.route("/thank-you")
+def thank_you():
+    return render_template('thank_you.html')
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
