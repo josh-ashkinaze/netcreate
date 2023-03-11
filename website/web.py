fbe9b590fcbd25564d9dc60be5c579142c18d21e
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-03-11 18:41:48 -0500

    - change 'trial' to 'condition_no' - make N_EXAMPLES a global variable paramter of experiment - clean up pulling in responses - add small sleep requests to make sure database query done before page loads (maybe not needed?)

--- website/web.py
+++ website/web.py
@@ -1,3 +1,7 @@
+
+# TODO clean up the database. As of now 'trial' is a dummy value. Don't need it  because it's just row anyway
+# TODO fix the specific items
+
 from flask import Flask, render_template, request, redirect, url_for
 from google.cloud import bigquery
 from google.oauth2 import service_account
@@ -6,29 +10,28 @@ import time
 from datetime import datetime
 import random
 
-# EXPERIMENT CONDITIONS
+# EXPERIMENT PARAMETERS
 ####################
 CONDITIONS = {
-    'hh':{'source':'human', 'label':'humans'},
-    'ha':{'source':'human', 'label':'artifical intelligence'},
-    'aa':{'source':'ai', 'label':'artifical intelligence'},
-    'ah':{'source':'ai', 'label':'humans'}
+    'hh': {'source': 'human', 'label': 'humans'},
+    'ha': {'source': 'human', 'label': 'artifical intelligence'},
+    'aa': {'source': 'ai', 'label': 'artifical intelligence'},
+    'ah': {'source': 'ai', 'label': 'humans'}
 }
 ITEMS = ['a rope', 'a bat', 'a ball', 'a coffee cup']
+
+N_EXAMPLES = 5
 ####################
 
 
-# GLOBAL VARIABLES FOR PARTICIPANT AND TRIAL
+# GLOBAL VARIABLES FOR PARTICIPANT AND condition_no
 temp = {
     "item_order": None,
     "condition_order": None,
     "participant_id": None,
 }
 
-
-
 app = Flask(__name__)
-
 
 key_path = "../creds/netcreate-0335ce05e7ff.json"
 credentials = service_account.Credentials.from_service_account_file(
@@ -39,9 +42,11 @@ client = bigquery.Client(credentials=credentials, project=credentials.project_id
 dataset = client.dataset("net_expr")
 table = dataset.table("trials")
 
+
 @app.route("/")
 def consent_form():
     return render_template('consent_form.html')
+
 
 @app.route("/start-experiment")
 def start_experiment():
@@ -52,42 +57,57 @@ def start_experiment():
     random.shuffle(condition_order)
     temp['condition_order'] = condition_order
     temp['item_order'] = item_order
-    temp['trial'] = 0
-    time.sleep(0.5)
-    return redirect(url_for('experiment', trial=temp['trial']))
+    time.sleep(0.05)
+    return redirect(url_for('experiment', condition_no=0))
 
 
+@app.route("/experiment/<int:condition_no>", methods=['GET', 'POST'])
+def experiment(condition_no):
+    """
+    Recursively handles the experiment route for a particular condition_no. 
+    
+    If the current condition_no number is more than the number of items, returns the thank you page.
+    
+    Otherwise, this function will:
+    
+        1. If the HTTP method is GET, the function retrieves the necessary context from the global temp dict and generates an experiment instance. 
 
-@app.route("/experiment/<int:trial>", methods=['GET', 'POST'])
-def experiment(trial):
-    # If the participant has completed all trials, redirect to thank you page
-    if trial > 3:
+        2. If the HTTP method is POST, the function retrieves the participant's response text and inserts it into a BigQuery table. Then we call `experiment(condition_no+1)'
+
+
+    Parameters:
+    - condition_no (int): the current condition_no
+
+    Returns:
+    - A Flask response object containing the rendered experiment template with the item, label, previous responses, and condition_no number as context variables.
+
+    """
+    # If the participant has completed all condition_nos, redirect to thank you page
+    time.sleep(0.1)
+    if condition_no > len(ITEMS)-1:
         return redirect(url_for('thank_you'))
 
     # Retrieve the necessary information from the global temp variable
-    label = CONDITIONS[temp['condition_order'][trial]]['label']
-    source = CONDITIONS[temp['condition_order'][trial]]['source']
+    label = CONDITIONS[temp['condition_order'][condition_no]]['label']
+    source = CONDITIONS[temp['condition_order'][condition_no]]['source']
     participant_id = temp['participant_id']
-    condition = temp['condition_order'][trial]
-    condition_order = trial
-    item = temp['item_order'][trial]
+    condition = temp['condition_order'][condition_no]
+    condition_order = condition_no
+    item = temp['item_order'][condition_no]
     id = str(uuid.uuid4())
 
+    # If the HTTP method is POST, insert the participant's response into the BigQuery table
     if request.method == 'POST':
-        # Get the response text and the datetime of the response
         response_text = request.form.get('participant_response')
-        print("RESPONSE TEXT", response_text)
-
-        # Insert the response into the trials table
         row = {
             "item": item,
             "id": id,
             "participant_id": participant_id,
-            "trial": trial,
+            "condition_no": condition_no,
             "response_text": response_text,
             "response_date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             "condition": condition,
-            "condition_order": condition_order,
+            "trial": 1,
             "source": source,
             "label": label,
         }
@@ -98,22 +118,21 @@ def experiment(trial):
         else:
             print("Encountered errors while inserting rows: {}".format(errors))
 
-        # Redirect to next trial
-        return redirect(url_for('experiment', trial=trial+1))
-
-    # Generate a prompt for the participant to respond to
-    rows = list(client.query(f"SELECT response_text FROM `net_expr.trials` WHERE item= '{item}' ").result())
-    responses = [row['response_text'] for row in rows][-3:]
-    print("responses", responses)
-    return render_template('experiment.html', item=item, label=label, rows=responses, trial=trial)
+        # Redirect to next condition_no
+        return redirect(url_for('experiment', condition_no=condition_no + 1))
+    
+    # If the HTTP method is GET, render the experiment template
+    else:
+        rows = list(client.query(f"SELECT response_text FROM `net_expr.trials` WHERE item= '{item}' ORDER BY response_date DESC LIMIT {N_EXAMPLES} ").result())
+        responses = [row['response_text'] for row in rows]
+        print("responses", responses)
+        return render_template('experiment.html', item=item, label=label, rows=responses, condition_no=condition_no)
 
 
 @app.route("/thank-you")
 def thank_you():
     return render_template('thank_you.html')
 
+
 if __name__ == '__main__':
     app.run(port=5008, debug=True)
-
-
-
