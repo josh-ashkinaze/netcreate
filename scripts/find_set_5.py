575ca7c70c8ad94d4f4729c8ef4627709470e13d
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-03-13 14:11:18 -0400

    Use a different dataset

--- scripts/find_set_5.py
+++ scripts/find_set_5.py
@@ -12,33 +12,52 @@ import itertools
 import logging
 import os
 import numpy as np
+import subprocess
+
+
 def main():
     log_file = os.path.splitext(os.path.basename(__file__))[0] + '.log'
     logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s %(message)s')
 
-    # Read in AUT scores from
-    # https://github.com/massivetexts/open-scoring/tree/master/data
-    #
-    # These scores are referenced in the paper:
-    # Organisciak, P., Acar, S., Dumas, D., & Berthiaume, K. (2022).
-    # Beyond Semantic Distance: Automated Scoring of Divergent Thinking Greatly Improves with Large Language Models.
-    # https://doi.org/10.13140/RG.2.2.32393.31840
-    df = pd.read_csv("../data/AUT_Coding_ColoradoScores_v2.csv")
-    df = df.dropna(subset=['Colorado Similarity Scores'])
-    df['sim'] = df['Colorado Similarity Scores']
-    df = df.query("sim>=0&sim<=1")
+    # Read in AUT scores that were used in the paper:
+    # Dumas, D., Organisciak, P., & Doherty, M. (2021). Measuring divergent thinking originality with human raters and text-mining models: A psychometric comparison of methods. Psychology of Aesthetics, Creativity, and the Arts, 15(4), 645–663.
 
-    # Now we want to find the set of 5 items with the lowest variance of creativity
-    means = df.groupby(by=['Prompt']).mean().reset_index()[['Prompt', 'sim']]
-    all_combos = list(itertools.combinations(means['Prompt'].tolist(), 5))
+    # Note: The link to the data was obtained by Joshua Ashkinaze contacting Peter Organisciak
+
+    # Check if the data file already exists
+    dest_path = "../data/dumas_et_al.csv"
+    if os.path.isfile(dest_path):
+        logging.info("Data file already exists. Skipping download step.")
+    else:
+        # Download the file
+        logging.info("Downloading data file...")
+        dumas_et_al_link = "https://osf.io/download/u3yv4/"
+        subprocess.call(["wget", "-O", dest_path, dumas_et_al_link])
+        logging.info("Data file downloaded.")
+
+    # Summary stats
+    df = pd.read_csv(dest_path)
+    summary = df.groupby(by=['prompt'])['human_vote'].describe()
+    summary['count'] = summary['count'].astype(int)
+    summary = summary.round(2).reset_index()
+    logging.info("\n" + summary.to_latex(index=False,
+                           caption='Summary statistics of originality scores (1-5) from \citet{dumas_measuring_2021}',
+                           label='tab:prior_work_prompts'))
+
+    # Get combo with smallest variance
+    means = df.groupby(by=['prompt']).mean().reset_index()[['prompt', 'human_vote']]
+    all_combos = list(itertools.combinations(means['prompt'].tolist(), 5))
     data = []
     for combo in all_combos:
-        combo_var = np.var(means[means['Prompt'].isin(combo)]['sim'].tolist())
+        combo_var = np.var(means[means['prompt'].isin(combo)]['human_vote'].tolist())
         data.append({'combo': str(combo), 'var': combo_var})
     combo_df = pd.DataFrame(data)
-    logging.info("Lowest variance subset of 5")
-    logging.info(combo_df.sort_values(by=['var'], ascending=True).head(1))
-    logging.info(combo_df['var'].describe())
+    combo_df = combo_df.sort_values(by=['var'], ascending=True)
+    logging.info("Head of DF")
+    logging.info(combo_df.sort_values(by=['var']).head(5))
+    logging.info("Most Sim Subset")
+    logging.info(combo_df.sort_values(by=['var']).head(1))
 
 
-main()
+if __name__ == "__main__":
+    main()
